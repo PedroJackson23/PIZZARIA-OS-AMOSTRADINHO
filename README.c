3f1c93533d4b138709c5035aecaf7df84152592c
jooh1010 <joadsonmrn100@gmail.com>
2024-09-21 07:04:59 -0300

    Update README.c

--- README.c
+++ README.c
@@ -4,39 +4,46 @@
 #include <locale.h>
 #include <time.h>
 
-void titulo(){
-printf("  ___ ___ ________  _   ___ ___   _      ___  ___     _   __  __  ___  ___ _____ ___   _   ___ ___ _  _ _  _  ___  ___ \n");
-printf(" | _ \\_ _|_  /_  / /_\\ | _ \\_ _| /_\\    / _ \\/ __|   /_\\ |  \\/  |/ _ \\/ __|_   _| _ \\ /_\\ |   \\_ _| \\| | || |/ _ \\/ __|\n");
-printf(" |  _/| | / / / / / _ \\|   /| | / _ \\  | (_) \\__ \\  / _ \\| |\\/| | (_) \\__ \\ | | |   // _ \\| |) | || .` | __ | (_) \\__ \\\n");
-printf(" |_| |___/___/___/_/ \\_\\_|_\\___/_/ \\_\\  \\___/|___/ /_/ \\_\\_|  |_|\\___/|___/ |_| |_|_\\_/ \\_\\___/___|_|\\_|_||_|\\___/|___/\n");
-
-
+void titulo() {
+    printf("  ___ ___ ________  _   ___ ___   _      ___  ___     _   __  __  ___  ___ _____ ___   _   ___ ___ _  _ _  _  ___  ___ \n");
+    printf(" | _ \\_ _|_  /_  / /_\\ | _ \\_ _| /_\\    / _ \\/ __|   /_\\ |  \\/  |/ _ \\/ __|_   _| _ \\ /_\\ |   \\_ _| \\| | || |/ _ \\/ __|\n");
+    printf(" |  _/| | / / / / / _ \\|   /| | / _ \\  | (_) \\__ \\  / _ \\| |\\/| | (_) \\__ \\ | | |   // _ \\| |) | || .` | __ | (_) \\__ \\\n");
+    printf(" |_| |___/___/___/_/ \\_\\_|_\\___/_/ \\_\\  \\___/|___/ /_/ \\_\\_|  |_|\\___/|___/ |_| |_|_\\_/ \\_\\___/___|_|\\_|_||_|\\___/|___/\n");
 }
 
 typedef struct {
-int id;
-char *nome;
-char *descricao;
-float preco;
-}Pizza;
+    int id;
+    char *nome;
+    char *descricao;
+    float preco;
+} Pizza;
 
 typedef struct {
-int id_pizza;
-char sabor[50];
-char rua[50][50];
-char bairro[50][50];
-int endereco;
-long int id_do_pedido;
-float preco;
-}Pedido;
+    int id;
+    char *nome;
+    float preco;
+} Refri;
 
-int main()
-{
+typedef struct {
+    int id_pizza;
+    char sabor[50];
+    char rua[50];
+    char bairro[50];
+    int endereco;
+    long int id_do_pedido;
+    float preco;
+    int id_bebida;
+    char bebida[50];
+} Pedido;
+
+int main() {
+    FILE *gravar_pedido;
     srand(time(NULL));
     setlocale(LC_ALL, "");
     titulo();
-Pedido pedido= {};
-Pizza cardapio[10] = {
+
+    Pedido pedido = {};
+    Pizza cardapio[15] = {
         {1, "Mussarela", "queijo mussarela, molho de tomate e oregano.", 30.50},
         {2, "Calabresa", "calabresa, cebola, muçarela, azeitonas e oregano.", 32.00},
         {3, "Portuguesa", "presunto, muçarela, ovos, cebola, pimentão, azeitonas e oregano.", 35.00},
@@ -46,39 +53,91 @@ Pizza cardapio[10] = {
         {7, "Peperoni", "fatias de peperoni, queijo mussarela e oregano.", 31.50},
         {8, "Vegetariana", "legumes variados, muçarela e oregano.", 32.50},
         {9, "Camarão", "camarão, muçarela e oregano.", 33.50},
-        {10, "Carioca", "bacon, batata frita, muçarela e oregano.", 34.50}
+        {10, "Carioca", "bacon, batata frita, muçarela e oregano.", 34.50},
+        {11, "Beijinho", "mucarela, chocolate branco e coco ralado.", 34.50},
+        {12, "Brigadeiro", "mucarela, creme de avela e brigadeiro.", 34.50},
+        {13, "Banana", "banana, chocolate e mucarela.", 34.50},
+        {14, "Morango", "Morango, chocolate e mucarela.", 34.50},
+        {15, "M&M", "m&m, chocolate e mucarela.", 34.50}
     };
+
+    Refri menu[10] = {
+        {1, "Coca-Cola 2L", 12.00},
+        {2, "Sucuzin Natural 2L", 5.00},
+        {3, "Fanta Uva 2L", 10.00},
+        {4, "Fanta Laranja 2L", 10.00},
+        {5, "Guaraná 2L", 10.00},
+        {6, "Pepsi 2L", 10.00},
+        {7, "Coca Lata", 5.00},
+        {8, "Guaraná Lata", 4.00},
+        {9, "Pepsi Lata", 4.00},
+        {10, "Sem bebida", 0.00}
+    };
+
     printf("\n\n");
-    printf("\n=================================================================================\n");
-    printf("                                  CARDAPIO                                        \n");
     printf("=================================================================================\n");
-    for(int i = 0; i < 10; i++){
-        printf("%d - ", i+1);
-        printf("%s", cardapio[i].nome);
-        printf("    %.2f\n", cardapio[i].preco);
-        printf("%s\n", cardapio[i].descricao);
-        printf("\n");
+    printf("                                  CARDÁPIO                                       \n");
+    printf("=================================================================================\n");
+    for (int i = 0; i < 15; i++) {
+        printf("%d - %s    %.2f\n%s\n\n", i + 1, cardapio[i].nome, cardapio[i].preco, cardapio[i].descricao);
     }
+
     printf("=================================================================================\n");
-    printf("qual pizza você deseja escolher? Digite o id: ");
+    printf("Qual pizza você deseja escolher? Digite o id: ");
     scanf("%d", &pedido.id_pizza);
-    for(int i = 0; i <10; i++){
-        if(cardapio[i].id == pedido.id_pizza){
+
+    for (int i = 0; i < 15; i++) {
+        if (cardapio[i].id == pedido.id_pizza) {
             strcpy(pedido.sabor, cardapio[i].nome);
             pedido.preco = cardapio[i].preco;
         }
     }
+
+    printf("\n\n");
+    printf("=================================================================================\n");
+    printf("                             CARDÁPIO DE BEBIDAS                                 \n");
+    printf("=================================================================================\n");
+    for (int j = 0; j < 10; j++) {
+        printf("%d - %s    %.2f\n", j + 1, menu[j].nome, menu[j].preco);
+    }
+
+    printf("=================================================================================\n");
+    printf("Qual bebida você deseja escolher? Digite o id: ");
+    scanf("%d", &pedido.id_bebida);
+
+    for (int j = 0; j < 10; j++) {
+        if (menu[j].id == pedido.id_bebida) {
+            strcpy(pedido.bebida, menu[j].nome);
+            pedido.preco += menu[j].preco;
+        }
+    }
+
     printf("Digite aqui seu Bairro: ");
     getchar();
-    scanf("%[^\n]s", pedido.bairro);
+    fgets(pedido.bairro, 50, stdin);
+    pedido.bairro[strcspn(pedido.bairro, "\n")] = 0;
+
     printf("Digite aqui sua Rua: ");
-    getchar();
-    scanf("%[^\n]s", pedido.rua);
-    printf("Digite aqui o numero de sua casa: ");
-    getchar();
+    fgets(pedido.rua, 50, stdin);
+    pedido.rua[strcspn(pedido.rua, "\n")] = 0;
+
+    printf("Digite aqui o número de sua casa: ");
     scanf("%d", &pedido.endereco);
+
     pedido.id_do_pedido = (rand() % 9000000) + 1000000;
 
+    gravar_pedido = fopen("gravar_pedido.txt", "w");
+    if (gravar_pedido == NULL) {
+        printf("Erro ao abrir o arquivo!\n");
+        return 1;
+    }
+
+    fprintf(gravar_pedido, "ID do Pedido: %ld\nID da Pizza: %d\nSabor: %s\nID da Bebida: %d\nBebida: %s\nRua: %s\nBairro: %s\nNúmero: %d\nPreço: %.2f\n",
+            pedido.id_do_pedido, pedido.id_pizza, pedido.sabor, pedido.id_bebida, pedido.bebida, pedido.rua, pedido.bairro, pedido.endereco, pedido.preco);
+
+    fclose(gravar_pedido);
+
+    printf("Informações gravadas com sucesso!\n");
 
     return 0;
 }
